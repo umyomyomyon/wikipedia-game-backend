581db6bb0e14121922b6c1183983453f0dd11003
umyomyomyon <minecraft.sentry.yym@gmail.com>
2022-01-17 22:19:15 +0900

    destroy_roomを実装

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from rooms import (create_room_id, init_room, _join_room, setting_article, change_room_status, change_player_progress,
-                   get_room_users, is_all_room_users_done)
+                   get_room_users, is_all_room_users_done, _destroy_room)
 from firestore import (record_player_progress, cancel_player_progress, record_game_result,
                        delete_all_document_in_collection)
 from exceptions import (RoomNotExistException, RoomIdDuplicateException, URLValidationException, NotInRoomUserException,
@@ -62,8 +62,6 @@ def end_room():
         record_game_result(room_id)
     except NotHostException as e:
         return jsonify({'message': e.message}), e.status_code
-    except NotInRoomUserException as e:
-        return jsonify({'message': e.message}), e.status_code
     except Exception as e:
         logger.log_struct(
             {'error': str(e)},
@@ -73,17 +71,19 @@ def end_room():
         return jsonify({'message': 'end room failed.'}), 400
 
 
-#
-# @app.route('/room', methods=['DELETE'])
-# def destroy_room():
-#     try:
-#         room_id = 11111
-#         _destroy_room(room_id)
-#         return jsonify({'message': 'room deleted.'}), 204
-#     except RoomNotExistException as e:
-#         return jsonify({'message': e.message}), e.status_code
-#     except Exception as e:
-#         return jsonify({'message': 'error'}), 400
+@app.route('/room', methods=['DELETE'])
+def destroy_room():
+    try:
+        data = request.get_json()
+        room_id, user_uuid = data.get('room_id'), data.get('uuid')
+        _destroy_room(room_id, user_uuid=user_uuid)
+        return jsonify({'message': 'room deleted.'}), 204
+    except RoomNotExistException as e:
+        return jsonify({'message': e.message}), e.status_code
+    except NotHostException as e:
+        return jsonify({'message': e.message}), e.status_code
+    except Exception as e:
+        return jsonify({'message': 'error destroy room.'}), 400
 
 
 @app.route('/room/join', methods=['POST'])
